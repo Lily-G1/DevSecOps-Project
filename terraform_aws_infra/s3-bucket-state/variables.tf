162c7a79abf428a53c51bf6009299d98b9268a29
Lilian A Galadima <104821662+Lily-G1@users.noreply.github.com>
2026-04-25 10:09:21 +0100

    Update variables.tf

--- terraform_aws_infra/s3-bucket-state/variables.tf
+++ terraform_aws_infra/s3-bucket-state/variables.tf
@@ -1,1 +1,6 @@
-["~/.aws/credentials3"]
+# AWS keypair
+variable "aws_credentials_path" {
+  type        = string
+  default     = "~/.aws/credentials3"
+  description = "path to AWS access credentials"
+} 
