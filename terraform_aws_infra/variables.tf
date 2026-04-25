bc0591226c1fac17ff1c3b7dd99e1366dc984142
Lilian A Galadima <104821662+Lily-G1@users.noreply.github.com>
2026-04-25 10:00:30 +0100

    Update variables.tf with aws credentials path

--- terraform_aws_infra/variables.tf
+++ terraform_aws_infra/variables.tf
@@ -1,6 +1,13 @@
 # AWS keypair
 variable "ssh_keypair" {
   type        = string
-  default     = "ann-web-server"
+  default     = "ann-web-serve"
   description = "name of AWS SSH key-pair"
+} 
+
+# AWS keypair
+variable "aws_credentials_path" {
+  type        = string
+  default     = "~/.aws/credentials3"
+  description = "path to AWS access credentials"
 } 
